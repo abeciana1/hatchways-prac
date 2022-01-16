c131291d8e325802033f75b9e68ff40daec56471
Alex <alex.beciana@gmail.com>
2022-01-15 20:53:47 -0500

    Began part-2 task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 
+const fetchWeatherData = () => {
+
+}
+
+
 function App() {
+
   return (
     <div className="App">
     </div>
